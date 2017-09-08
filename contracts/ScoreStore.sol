747c01f5784e01179125dcd8e3e87b2a5aa018d5
Thiago Gouvea <tsmedia.rj@gmail.com>
2017-09-08 13:17:20 -0300

    first commit

---
+++ contracts/ScoreStore.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.4;
+
+contract ScoreStore{
+    mapping(string => int) PersonScores;
+    function AddPersonScore(string name, int startingScore){
+      if (PersonScores[name]>0){
+        throw;
+      }
+      else{
+        PersonScores[name] = startingScore;
+      }
+
+    }
+
+    function GetScores(string name) returns (int){
+      return PersonScores[name];
+    }
+
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,4 @@
+var ScoreStore= artifacts.require("./ScoreStore.sol");
+module.exports = function(deployer) {
+  deployer.deploy(ScoreStore);
+};
