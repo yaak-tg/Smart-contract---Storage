78ed2cb25c1085c450bfb3fcd430cfa5a67fd8ef
Thiago Gouvea <tsmedia.rj@gmail.com>
2017-09-08 13:51:23 -0300

    add comments PTBR to ScoreStore.sol

--- contracts/ScoreStore.sol
+++ contracts/ScoreStore.sol
@@ -1,5 +1,20 @@
 pragma solidity ^0.4.4;
 
+
+/*
+Este contrato vai Salvar nome e score de jogadores.
+1- crie um mapping (dicionário) Chamado PersonScores, que vai guardar uma string (nome do jogador) e um int (score);
+2 - Crie uma função AddPersonScore que:
+
+  a. recebe dois parametros: uma string name e um int startingScore;
+  b. Verifica se o jogador tem score maior que 0;
+  c. Atribui o score ao jogador
+
+3 - Crie uma função para verificar o score que:
+  a. Recebe uma string name e retorna um int(score);
+
+mapping é um dicionário
+*/
 contract ScoreStore{
     mapping(string => int) PersonScores;
     function AddPersonScore(string name, int startingScore){
